0154b46e9c4d9755394b791b783d597844f6f289
Jiseok97 <wltjr1426@naver.com>
2023-03-01 19:49:00 +0900

    [Feat] Main Storyboard 제거 및 MainViewController 적용

---
+++ PreOnboarding/Sources/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  PreOnboarding
+//
+//  Created by 이지석 on 2023/03/01.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("DEUBG: start main view controller")
+        view.backgroundColor = .link
+    }
+}
