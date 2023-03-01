788e81a11c35d436d35c3a627abbed9138dad8f5
Jiseok97 <wltjr1426@naver.com>
2023-03-01 21:51:40 +0900

    [Feat] FetchImageViewModel 생성

---
+++ PreOnboarding/Sources/ViewModel/FetchImageViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  FetchImageViewModel.swift
+//  PreOnboarding
+//
+//  Created by 이지석 on 2023/03/01.
+//
+
+import Foundation
+import UIKit
+
+final class FetchImageViewModel {
+    
+    func fetchImage(
+        _ url: URL,
+        _ completion: @escaping (UIImage?) -> Void
+    ) -> URLSessionDataTask {
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            let image = UIImage(data: data)
+            completion(image)
+        }
+        task.resume()
+        return task
+    }
+}
